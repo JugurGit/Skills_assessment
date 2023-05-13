da431afe63c0a4cf6ceb7e884c6e75f709b2348d
JugurGit <56389749+JugurGit@users.noreply.github.com>
2023-05-13 16:57:37 +0200

    Add files via upload
    rajout base de données + fonctionnalités edit delete create

---
+++ quiz 2 LE BEST V2/app_qcm.py
@@ -0,0 +1,100 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+#https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
+
+app = Flask(__name__)
+app._static_folder = "C:/Users/jugur/Documents/projet Git/quiz 2 LE BEST/templates/static"
+
+app.config['SECRET_KEY'] = 'test'
+
+def get_db_connection():
+    conn = sqlite3.connect('database_qcm.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM questions WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+def get_option(question_id):
+    conn = get_db_connection()
+    option = conn.execute('SELECT * FROM options WHERE question_id = ?',
+                        (question_id,)).fetchall()
+    conn.close()
+    if post is None:
+        abort(404)
+    return option
+
+@app.route('/')
+def start():
+    return render_template('accueil_qcm.html')
+
+@app.route('/admin')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM questions').fetchall()
+    conn.close()
+    return render_template('index_qcm.html', posts=posts)
+
+@app.route('/admin/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    option = get_option(post_id)
+    return render_template('post_qcm.html', post=post, option=option)
+
+@app.route('/admin/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Question is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO questions (qcm, question, answer) VALUES (?, ?, ?)',
+                          (0, title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create_qcm.html')
+
+@app.route('/admin/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE questions SET question = ?, answer = ?'
+                          ' WHERE id = ?',
+                          (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit_qcm.html', post=post)
+
+@app.route('/admin/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM questions WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['question']))
+    return redirect(url_for('index'))
+
+app.run(debug = False)
---
+++ quiz 2 LE BEST V2/init_db.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  8 22:43:32 2023
+
+@author: jugur
+"""
+
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
